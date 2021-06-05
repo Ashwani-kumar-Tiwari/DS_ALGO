45593546654529d2e5501badccd256e9c343ba9d
Ashwani Kumar Tiwari <tiwariashwani3012@gmail.com>
2021-06-05 17:54:39 +0530

    linked list quesstions

--- Linked_List/ques.java
+++ Linked_List/ques.java
@@ -1,4 +1,4 @@
-class linkedlist{
+class linkedLists{
     private class Node{
         private int data;
         private Node next;
@@ -191,29 +191,152 @@ class linkedlist{
         return str;
     }
 
+    public void reverseDI() {
+        // write your code here
+        int left = 0;
+        int right = this.size - 1;
+
+        while(left < right){
+            Node lnode = getNodeAt(left);
+            Node rnode = getNodeAt(right);
+
+            //data swaping
+            int temp = lnode.data;
+            lnode.data = rnode.data;
+            rnode.data = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public void reversePI() {
+        // write your code here
+        Node prev = null;
+        Node curr = head;
+
+        while(curr != null){
+            Node next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        // swapping of head and tail pointer
+        Node temp = head;
+        head = tail;
+        tail = temp; 
+    }
+
+    public int kthFromLast(int k){
+        // write your code here
+        Node slow = head;
+        Node fast = head;
+        
+        //1. move fask at k step away from head
+        for(int i = 0; i < k; i++){
+            fast = fast.next;
+        }
+        
+        //2. move slow and fast simultaneously until fast.next != null
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return slow.data;
+    }
+
+    public int mid1(){
+        // write your code here
+        if(head == null) return -1;
+
+        Node slow = head;
+        Node fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next; // speed x
+            fast = fast.next.next;// speed 2x
+        }
+
+        return slow.data;
+    }
+
+    public int mid2(){
+        // write your code here
+        if(head == null) return -1;
+
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next; // speed x
+            fast = fast.next.next;// speed 2x
+        }
+
+        return slow.data;
+    }
+
+    public static linkedLists mergeTwoSortedLists(linkedLists l1, linkedLists l2) {
+        // write your code hered
+        linkedLists res = new  linkedLists();
+
+        Node p1 = l1.head;
+        Node p2 = l2.head;
+
+        while(p1 != null && p2 != null){
+            int val1 = p1.data;
+            int val2 = p2.data;
+
+            if(val1 < val2){
+                res.addLast(val1);
+                p1 = p1.next;
+            } else {
+                res.addLast(val2);
+                p2 = p2.next;
+            }
+        }
+
+        while(p1 != null){
+            p1.addLast(p1.data);
+            p1 = p1.next;
+        }
+
+        while(p2 != null){
+            p2.addLast(p2.data);
+            p2 = p2.next;
+        }
+        return res;
+    }
 }
-
-public class myLinkedList{
-
-    public static void demo(){
-        linkedlist list = new  linkedlist();
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
-        list.addLast(40);
-        list.addLast(50);
-        list.addLast(60);
-        System.out.println(list + "\n" + list.size());
-        list.addAt(35, 3);
-        System.out.println(list + "\n" + list.size());
-        System.out.println(list.removeFirst());
-        System.out.println(list + "\n" + list.size());
-        System.out.println(list.removeLast());
-        System.out.println(list + "\n" + list.size());
-
-        //list.display();
-    }
-    public static void main(String[] args) {
-        demo();
-    }
+      public class ques{
+      public static void main(String[] args) throws Exception {
+        linkedLists list1 = new  linkedLists();
+        linkedLists list2 = new  linkedLists();
+        list1.addLast(10);
+        list1.addLast(20);
+        list1.addLast(30);
+        list1.addLast(40);
+        list1.addLast(50);
+        list1.addLast(60);
+        list1.addLast(70);
+        
+        list2.addLast(15);
+        list2.addLast(25);
+        list2.addLast(35);
+        list2.addLast(45);
+        list2.addLast(55);
+        list2.addLast(65);
+        list2.addLast(75);
+        // System.out.println(list + "\n" + list.size());
+        // list.reverseDI(); //Reverse A Linked List (data Iterative)
+        // list.reversePI(); //Reverse Linked List (pointer Iterative)
+        // System.out.println(list.kthFromLast(3)); //Kth Node From End Of Linked List
+        // System.out.println(list.mid1()); //Mid Of Linked List
+        // System.out.println(list.mid2()); //Mid Of Linked List
+        linkedLists res = mergeTwoSortedLists(list1, list2);
+        System.out.println(list1 + "\n" + list1.size());
+        System.out.println(list2 + "\n" + list2.size());
+        System.out.println(res + "\n" + res.size());
+      }
 }
